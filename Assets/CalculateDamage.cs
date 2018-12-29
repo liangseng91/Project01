eb55465ca325403a3c714da19d3395564f6e4850
liangseng91 <42112011+liangseng91@users.noreply.github.com>
2018-12-30 07:55:36 +0800

    update

---
+++ Assets/CalculateDamage.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CalculateDamage : MonoBehaviour {
+
+    public float baseDamage;
+    public float dmgMultiplier;
+
+	// Use this for initialization
+	void Start () {
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+    }
+
+    public int CalculateFinal()
+    {
+        float finalDamage = baseDamage * dmgMultiplier;
+        int finalDamage_Rounded = Mathf.RoundToInt(finalDamage);
+        return finalDamage_Rounded;
+    }
+}
---
+++ Assets/DamageHandler.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageHandler : MonoBehaviour {
+
+    void Attack(Object objectName) {
+        Instantiate(objectName,transform.position, transform.rotation);
+    }
+}
