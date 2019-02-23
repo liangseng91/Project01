75ab869a9a62505cef6280ce805c4b9696c7993f
liangseng91 <42112011+liangseng91@users.noreply.github.com>
2019-02-23 11:21:26 +0800

    2/23/2019
    Steering & Acceleration controls

--- Assets/Standard Assets/Vehicles/Car/Scripts/CarUserControl.cs
+++ Assets/Standard Assets/Vehicles/Car/Scripts/CarUserControl.cs
@@ -8,7 +8,7 @@ namespace UnityStandardAssets.Vehicles.Car
     public class CarUserControl : MonoBehaviour
     {
         private CarController m_Car; // the car controller we want to use
-
+        private float v;
 
         private void Awake()
         {
@@ -17,14 +17,20 @@ namespace UnityStandardAssets.Vehicles.Car
         }
 
 
-        private void FixedUpdate()
+        private void Update()
         {
             // pass the input to the car!
             float h = CrossPlatformInputManager.GetAxis("Horizontal");
-            float v = CrossPlatformInputManager.GetAxis("Vertical");
+            if (CrossPlatformInputManager.GetButtonDown("Vertical")) {
+                v = 1;
+            }
+            else if (CrossPlatformInputManager.GetButtonUp("Vertical")) {
+                v = 0;
+            }
+            float v2 = CrossPlatformInputManager.GetAxis("Vertical2");
 #if !MOBILE_INPUT
             float handbrake = CrossPlatformInputManager.GetAxis("Jump");
-            m_Car.Move(h, v, v, handbrake);
+            m_Car.Move(h, v, v2, handbrake);
 #else
             m_Car.Move(h, v, v, 0f);
 #endif
