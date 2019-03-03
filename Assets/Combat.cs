8117204a8b021806392f5c2567f926ebb04a0d33
liangseng91 <42112011+liangseng91@users.noreply.github.com>
2019-03-03 23:57:49 +0800

    update

---
+++ Assets/Combat.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Combat : MonoBehaviour {
+
+    private bool keyPressed = false;
+    Animator m_Animator;
+
+    private void Start()
+    {
+        m_Animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        if (m_Animator.GetInteger("attackCombo") == 0) {
+            if (Input.GetKeyDown("q"))
+            {
+                m_Animator.SetTrigger("Attack");
+            }
+        }
+    }
+
+    public void WaitCombo() {
+        StartCoroutine(WaitInput());
+    }
+
+    private IEnumerator WaitInput() {
+        while (!keyPressed) {
+            if (Input.GetKeyDown("q")) {
+                NextCombo();
+                break;
+            }
+        }
+        yield return 0;
+    }
+
+    private void NextCombo() {
+        keyPressed = true;
+        int i = m_Animator.GetInteger("attackCombo");
+        m_Animator.SetInteger("attackCombo", i + 1);
+        
+    }
+
+    public void ResetCombo()
+    {
+        m_Animator.SetInteger("attackCombo", 0);
+
+    }
+}
