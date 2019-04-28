c75cc557a268be4ba6d757ffd82f1c17fde76e1d
liangseng91 <42112011+liangseng91@users.noreply.github.com>
2019-04-28 14:48:05 +0800

    start

---
+++ Assets/FauxGravityBody.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FauxGravityBody : MonoBehaviour
+{
+    public FauxGravityAttractor attractor;
+    private Transform myTransform;
+    private Rigidbody myRigidbody;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        attractor = GameObject.FindObjectOfType<FauxGravityAttractor>();
+        myRigidbody = GetComponent<Rigidbody>();
+        myRigidbody.constraints = RigidbodyConstraints.FreezeRotation;
+        myRigidbody.useGravity = false;
+        myTransform = transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        attractor.Attract(myTransform);
+    }
+}
